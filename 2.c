a0427a4fc1a7fd8d538eae0169bd47e625288f3f
Chirag1292 <87476611+Chirag1292@users.noreply.github.com>
2023-10-10 12:25:58 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -3,7 +3,6 @@ int main(int argc, char const *argv[])
 {   int i;
     for(i=32;i<=126;i++){
         printf("%d is the ASCII of %c \n ",i,(char)i);
-        printf("%d is the ASCII of %c \n ",i,(char)i);
 
     }
     return 0;
